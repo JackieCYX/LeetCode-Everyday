155ffe2fb3d558c2d2a76ab81ff7d17c46ae4766
JordanCYX <2338257659@qq.com>
2023-12-08 19:21:28 +0800

    主分支结构搭建

---
+++ src/com/example/leetcode/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.example.leetcode;
+
+/**
+ * 初始化结构补充提交
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
